e376b08a64931d0e1e91e185c9a8bf25aae8103b
Mike Kurkin <mike@kurkin.net>
2020-09-18 09:40:46 +0300

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Mike Kurkin on 18.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
